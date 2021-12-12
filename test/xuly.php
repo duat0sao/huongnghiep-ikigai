a5c3c39b6783cc382526a77ede53d8f0fcdf157e
hjewvjpnd2k <hjewvjp2000@gmail.com>
2021-12-12 18:26:25 +0700

    update

--- test/xuly.php
+++ test/xuly.php
@@ -11,12 +11,11 @@ $nhom3 = $_POST['nhom3'];
 
 $nhom4 = $_POST['nhom4'];
 
-$idkhach = $_POST['idkhach'];
 
 
-
-$sql = "UPDATE disc SET nhom1='$nhom1', nhom2='$nhom2', nhom3='$nhom3', nhom4='$nhom4' WHERE idkhach='$idkhach'";
+ $sql = "INSERT INTO disc (nhom1, nhom2, nhom3, nhom4) VALUES ('$nhom1', '$nhom2', '$nhom3', '$nhom4')";
 
 mysqli_query($conn, $sql)
+
 
 ?>
