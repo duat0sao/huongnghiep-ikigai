33588d0ef7669bedf574efc1fd09208b2821a9bd
huskywannafly3081 <1851061357@e.tlu.edu.vn>
2021-12-17 21:39:46 +0700

    bosung data

--- config.php
+++ config.php
@@ -1,32 +1,17 @@
 <?php
 
-#define('HOST', 'localhost');
-#define('USERNAME', 'epiz_30592060');
-#define('PASSWORD', 'T9bLtp7efyXcZY');
-#define('DATABASE', 'epiz_30592060_huongnghiep');
-# 
-#
-#$conn=mysqli_connect('localhost', 'root', '', 'huongnghiep');
-#if(!$conn){
-#    die("connect fail ".mysqli_connect_error());
-#}
+define('HOST', 'sql313.epizy.com');
+define('USERNAME', 'epiz_30592060');
+define('PASSWORD', 'T9bLtp7efyXcZY');
+define('DATABASE', 'epiz_30592060_huongnghiep');
+ 
 
-
-
-define('HOST', 'localhost');
-define('USERNAME', 'root');
-define('PASSWORD', '');
-define('DATABASE', 'huongnghiep');
-
-
-$conn=mysqli_connect('localhost', 'root', '', 'huongnghiep');
+$conn=mysqli_connect('sql313.epizy.com', 'epiz_30592060', 'T9bLtp7efyXcZY', 'epiz_30592060_huongnghiep');
 mysqli_set_charset($conn, 'UTF8');
+
 if(!$conn){
     die("connect fail ".mysqli_connect_error());
 }
-
-
-
 
 
 ?>
