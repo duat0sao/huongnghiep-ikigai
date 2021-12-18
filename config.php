38f1faf78bf1eb0be27080c09581cc7370aa27bf
huskywannafly3081 <1851061357@e.tlu.edu.vn>
2021-12-18 09:49:48 +0700

    oke da oke css

--- config.php
+++ config.php
@@ -1,17 +1,31 @@
 <?php
 
-define('HOST', 'sql313.epizy.com');
-define('USERNAME', 'epiz_30592060');
-define('PASSWORD', 'T9bLtp7efyXcZY');
-define('DATABASE', 'epiz_30592060_huongnghiep');
+//define('HOST', 'sql313.epizy.com');
+//define('USERNAME', 'epiz_30592060');
+//define('PASSWORD', 'T9bLtp7efyXcZY');
+//define('DATABASE', 'epiz_30592060_huongnghiep');
+// 
+//
+//$conn=mysqli_connect('sql313.epizy.com', 'epiz_30592060', 'T9bLtp7efyXcZY', 'epiz_30592060_huongnghiep');
+//mysqli_set_charset($conn, 'UTF8');
+//
+//if(!$conn){
+//    die("connect fail ".mysqli_connect_error());
+//}
+
+
+
+define('HOST', 'localhost');
+define('USERNAME', 'root');
+define('PASSWORD', '');
+define('DATABASE', 'huongnghiep');
  
 
-$conn=mysqli_connect('sql313.epizy.com', 'epiz_30592060', 'T9bLtp7efyXcZY', 'epiz_30592060_huongnghiep');
+$conn=mysqli_connect('localhost', 'root', '', 'huongnghiep');
 mysqli_set_charset($conn, 'UTF8');
 
 if(!$conn){
     die("connect fail ".mysqli_connect_error());
 }
-
 
 ?>
